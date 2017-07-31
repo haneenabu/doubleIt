3e7641334c0b32d9854dcc7d85f1182959e06697
Haneen Abu-Khater <abukhater.haneen@gmail.com>
2017-07-30 17:36:13 -0700

    double number proj

---
+++ src/main/java/DoubleIt.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DoubleIt {
+    public static void main(String[] args) {
+        System.out.println("Give me a number and I will double it for you!");
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String stringNumber = bufferedReader.readLine();
+            Integer yourNumber = Integer.parseInt(stringNumber);
+            Integer yourDoubledNumber = yourNumber * 2;
+
+            System.out.println("I doubled your number for you: " + yourDoubledNumber);
+
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
